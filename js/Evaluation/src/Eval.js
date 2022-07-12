cf8382d7839af42e5a57c1edc3a01e8a2368a1e1
St3phane <bilhautsteph@gmail.com>
2022-07-12 11:05:19 +0200

    correction

--- js/Evaluation/src/Eval.js
+++ js/Evaluation/src/Eval.js
@@ -3,19 +3,23 @@ var a=0;
 var v=0;
 var personnes;
 do {
-  personnes = window.prompt("Entrez votre age");
+  personnes = window.prompt("Entrez un age (la saisie s'arrete a l'entrée d'un centenaire)");
   if ( personnes <20) {
     j++;
     console.log(personnes);
   }
-  else if ((personnes >=20 ) && (personnes <=40)) {
+  if ((personnes >=20 ) && (personnes <=40)) {
+    a++;
+    console.log(a);
+  }
+  if (personnes >=40) {
     v++;
     console.log(v);
   }
 }
 while (personnes < 100);
- 
-window.alert("Il y a " + "" + j + "" + " jeunes\n " + "Il y a " + "" + a + "" + " adulte\n " + " Il y a " + " Il y a " + "" + v + "" +" vieux\n " + " dont "+ " 1 " + "" + " centenaire ");
+
+window.alert("Il y a " + "" + j + "" + " jeunes\n " + "Il y a " + "" + a + "" + " adultes\n " + " Il y a " + " Il y a " + "" + v + "" +" vieux\n " + " dont "+ " 1 " + "" + " centenaire ");
 
 
 
@@ -23,7 +27,7 @@ window.alert("Il y a " + "" + j + "" + " jeunes\n " + "Il y a " + "" + a + "" + 
 
 var nombre = prompt("Entrer un chiffre");
 for (i = 1; i <= 10; i++) {
-document.write(i+ " x " +nombre+ " = "+i*nombre+"<br>");
+document.write(i+ " x " +nombre+ " = "+i*nombre+"");
 }
 
 //eval 03//
